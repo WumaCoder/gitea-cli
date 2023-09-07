15fb1ed9e9353f98e1f4ac7e533f573526699d58
WumaCoder <15804854160@163.com>
2023-09-07 19:11:04 +0800

    fix: 修复细节

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
-const { giteaApi } = require('gitea-js');
-const fetch = require('cross-fetch'); // You have to use a fetch compatible polyfill like cross-fetch for Node.JS
-const { Command } = require('commander');
-const fs = require('fs-extra')
-const simpleGit = require('simple-git');
-
+const { giteaApi } = require("gitea-js");
+const fetch = require("cross-fetch"); // You have to use a fetch compatible polyfill like cross-fetch for Node.JS
+const { Command } = require("commander");
+const fs = require("fs-extra");
+const simpleGit = require("simple-git");
 
 async function main() {
   const program = new Command();
 
-  program
-    .name('gitea-cli')
-    .description('CLI tool by gitea.')
-    .version('0.0.4');
+  program.name("gitea-cli").description("CLI tool by gitea.").version("0.0.4");
 
-  program.command('sync')
-    .description('notify remote repository sync')
-    .argument('<string>', 'gitea repository url')
-    .option('-t, --token <string>', 'gitea token')
-    .option('-o, --owner <string>', 'user or org name')
-    .option('-r, --repo <string>', 'repo name')
+  program
+    .command("sync")
+    .description("notify remote repository sync")
+    .argument("<string>", "gitea repository url")
+    .option("-t, --token <string>", "gitea token")
+    .option("-o, --owner <string>", "user or org name")
+    .option("-r, --repo <string>", "repo name")
     .action(async (str, options) => {
-        console.log(`[sync]: ${str}`)
-        const url = new URL(str)
-        const api = giteaApi(url.origin, {
-          token:  options.token, // generate one at https://gitea.example.com/user/settings/applications
-          customFetch: fetch,
-        });
-        
-        await api.repos.repoMirrorSync(options.owner, options.repo);
-        
-        console.log(`[sync]: ok`)
+      console.log(`[sync]: ${str}`);
+      const url = new URL(str);
+      const api = giteaApi(url.origin, {
+        token: options.token, // generate one at https://gitea.example.com/user/settings/applications
+        customFetch: fetch,
+      });
+
+      await api.repos.repoMirrorSync(options.owner, options.repo);
+
+      console.log(`[sync]: ok`);
     });
 
-  const originSub = program.command('origin')
+  const originSub = program.command("origin");
   originSub
-    .command('pull')
-      .description('pull origin repo.')
-      .argument('<string>', 'clone dir path')
-      .option('-r, --repo <string>', 'git url')
-      .option('-b, --branch <string>', 'git branch')
-      .action(async (cwd, options) => {
-        console.log(`[origin][pull]: ${cwd} ${options.branch}`)
-        fs.mkdirpSync(cwd);
-        if(fs.existsSync(`${cwd}/.git`)){
-          console.log('pull')
-          await simpleGit(cwd).checkout(options.branch).pull('origin', options.branch);
-        }else{
-          console.log('clone')
-          await simpleGit().clone(options.repo, cwd);
-        }
-        console.log(`[origin][pull]: ok`)
-      });
+    .command("pull")
+    .description("pull origin repo.")
+    .argument("<string>", "clone dir path")
+    .option("-r, --repo <string>", "git url")
+    .option("-b, --branch <string>", "git branch")
+    .action(async (cwd, options) => {
+      console.log(`[origin][pull]: ${cwd} ${options.branch || "default"}`);
+      fs.mkdirpSync(cwd);
+      if (fs.existsSync(`${cwd}/.git`)) {
+        console.log("pull");
+        const git = simpleGit(cwd);
+        await git.checkout(options.branch);
+        await git.pull("origin", options.branch);
+      } else {
+        console.log("clone");
+        await simpleGit().clone(options.repo, cwd);
+      }
+      console.log(`[origin][pull]: ok`);
+    });
 
   await program.parseAsync();
 }
 
-main().catch(console.error)
+main().catch(console.error);
 
 // example: node index.js sync http://localhost:3000/wumacoder/gitea-repo.git -t e6fe15f4685f4dba6be2004b4a768e598dfc973a -o wumacoder -r gitea-repo
